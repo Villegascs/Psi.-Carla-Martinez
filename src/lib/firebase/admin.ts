0a16c510ca93062f5df0c8505dac8329dc4fbf00
Villegas <ftsaevc@gmail.com>
2026-08-13 23:54:10 -0400

    fix: modular firebase admin imports properly externalized

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,11 +1,13 @@
-import admin from 'firebase-admin';
+import { initializeApp, getApps, cert } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import { getAuth } from 'firebase-admin/auth';
 
 function initFirebase() {
-  if (!admin.apps?.length) {
+  if (!getApps().length) {
     const privateKey = (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n').replace(/^"|"$/g, '');
     
-    admin.initializeApp({
-      credential: admin.credential.cert({
+    initializeApp({
+      credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: privateKey,
@@ -16,10 +18,10 @@ function initFirebase() {
 
 export const getAdminDb = () => {
   initFirebase();
-  return admin.firestore();
+  return getFirestore();
 };
 
 export const getAdminAuth = () => {
   initFirebase();
-  return admin.auth();
+  return getAuth();
 };
